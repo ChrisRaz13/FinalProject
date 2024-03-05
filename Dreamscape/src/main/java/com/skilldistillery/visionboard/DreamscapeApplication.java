a1a2c07e7d73070e75efc458804da6db1db44a76
Christopher Rasmussen <Chris.E.Rasmussen1@gmail.com>
2024-03-06 01:43:33 +0800

    Create Spring Boot Rest, connect JPA project dependency

---
+++ Dreamscape/src/main/java/com/skilldistillery/visionboard/DreamscapeApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.visionboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamscapeApplication.class, args);
+	}
+
+}
---
+++ Dreamscape/src/test/java/com/skilldistillery/visionboard/DreamscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.visionboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
