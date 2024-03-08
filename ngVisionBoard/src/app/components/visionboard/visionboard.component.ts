a52e4c3437209fda2a896b87dd71de6a17e8213a
Ieshia Parker <ieshiak@icloud.com>
2024-03-08 18:22:46 -0500

    VisionBoard Component

--- ngVisionBoard/src/app/components/visionboard/visionboard.component.ts
+++ ngVisionBoard/src/app/components/visionboard/visionboard.component.ts
@@ -1,6 +1,8 @@
+import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Board } from '../../models/board';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -10,12 +12,38 @@ import { Component } from '@angular/core';
   templateUrl: './visionboard.component.html',
   styleUrl: './visionboard.component.css'
 })
-export class VisionboardComponent {
+export class VisionboardComponent implements OnInit{
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+    this.fetchBoardData();
+  }
 
   items = ['Item 1', 'Item 2', 'Item 3'];
 
-drop(event: CdkDragDrop<string[]>): void {
-  moveItemInArray(this.items, event.previousIndex, event.currentIndex);
-}
+  board: Board | undefined;
+  userId: number = 1; // Assuming there's a logged-in user
 
+  constructor(private http: HttpClient) {}
+
+  drop(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
+
+  // testing
+
+  fetchBoardData() {
+    this.http.get<Board>('/get-board/' + this.userId).subscribe(response => {
+      this.board = response;
+    });
+  }
+
+  onFileDrop(event: any) {
+    // Handle file drop here
+  }
+
+  saveBoard() {
+    this.http.post('/save-board', { userId: this.userId, boardData: this.board }).subscribe(response => {
+      console.log('Board saved successfully');
+    });
+  }
 }
