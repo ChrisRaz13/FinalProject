aca7bffff956bfa6874ed0e1121b7b91ce067e89
Ieshia Parker <ieshiak@icloud.com>
2024-03-09 06:48:29 -0500

    updated userService

--- ngVisionBoard/src/app/services/user.service.ts
+++ ngVisionBoard/src/app/services/user.service.ts
@@ -1,90 +1,78 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { User } from '../models/user';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
+import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  private url = environment.baseUrl + 'api/users'
+  private url = environment.baseUrl + 'api/users';
 
-  constructor(private http: HttpClient, private auth: AuthService) { }
+  constructor(private http: HttpClient, private auth: AuthService) {}
 
+  getHttpOptions() {
+    return {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+  }
 
   index(): Observable<User[]> {
-    return this.http.get<User[]>(this.url, this.getHttpOptions()).pipe(
+    return this.http.get<User[]>(`${this.url}`, this.getHttpOptions()).pipe(
       catchError((err: any) => {
-        console.log(err);
-        return throwError(
-          () => new Error('USerService.index(): error retrieving user: ' + err)
-        );
+        console.error('Error retrieving users', err);
+        return throwError(() => new Error('UserService.findAllUsers(): error retrieving users: ' + err));
       })
     );
   }
 
-  show(id: number): Observable<User> {
-    return this.http.get<User>(`${this.url}/${id}`, this.getHttpOptions()).pipe(
+  findUserById(userId: number): Observable<User> {
+    return this.http.get<User>(`${this.url}/${userId}`, this.getHttpOptions()).pipe(
       catchError((err: any) => {
-        console.error(err);
-        return throwError(
-           () => new Error( 'UserService.show(): error showing Todo: ' + err )
-        );
+        console.error('Error retrieving user', err);
+        return throwError(() => new Error('UserService.findUserById(): error retrieving user: ' + err));
       })
     );
   }
 
-  create(user: User): Observable<User> {
-    return this.http.post<User>(this.url, user, this.getHttpOptions()).pipe(
+  update(userId: number, user: User): Observable<User> {
+    return this.http.put<User>(`${this.url}/${userId}`, user, this.getHttpOptions()).pipe(
       catchError((err: any) => {
-        console.error(err);
-        return throwError(
-           () => new Error( 'UserService.create(): error creating User: ' + err )
-        );
+        console.error('Error updating user', err);
+        return throwError(() => new Error('UserService.updateUser(): error updating user: ' + err));
       })
     );
   }
 
-  destroy(id: number): Observable<void> {
-    const deleteUrl = `${this.url}/${id}`;
-    return this.http.delete<void>(deleteUrl, this.getHttpOptions()).pipe(
-      catchError((error: any) => {
-        console.error(error);
-        return throwError(
-           () => new Error( 'UserService.destroy(): error deleting User: ' + error )
-        );
-      })
-    );
-  }
-
-  update(user: User): Observable<User> {
-    const updateUrl = `${this.url}/${user.id}`;
-    return this.http.put<User>(updateUrl, user, this.getHttpOptions()).pipe(
-      catchError((error: any) => {
-        console.error(error);
-        return throwError(
-           () => new Error( 'UserService.update(): error updating User: ' + error )
-        );
-      })
-    );
-  }
-
-  deactivateUser(id: number): Observable<any> {
-    return this.http.put(`${this.url}${id}/disable`, {}, this.getHttpOptions()).pipe(
+  destroy(userId: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${userId}`, this.getHttpOptions()).pipe(
       catchError((err: any) => {
-        console.error('UserService.deactivateUser(): error deactivating user', err);
-        return throwError(() => new Error('UserService.deactivateUser(): error deactivating user: ' + err));
+        console.error('Error deleting user', err);
+        return throwError(() => new Error('UserService.deleteUser(): error deleting user: ' + err));
       })
     );
   }
 
-  activateUser(id: number): Observable<any> {
-    return this.http.put(`${this.url}${id}/enable`, {}, this.getHttpOptions()).pipe(
+  activateUser(userId: number): Observable<User> {
+    return this.http.put<User>(`${this.url}/${userId}/enable`, {}, this.getHttpOptions()).pipe(
       catchError((err: any) => {
-        console.error('UserService.activateUser(): error activating user', err);
-        return throwError(() => new Error('UserService.activateUser(): error activating user: ' + err));
+        console.error('Error enabling user account', err);
+        return throwError(() => new Error('UserService.activateUser(): error activating user account: ' + err));
+      })
+    );
+  }
+
+  deactivateUser(userId: number): Observable<User> {
+    return this.http.put<User>(`${this.url}/${userId}/disable`, {}, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.error('Error disabling user account', err);
+        return throwError(() => new Error('UserService.deactivateUser(): error deactivating user account: ' + err));
       })
     );
   }
@@ -96,15 +84,5 @@ export class UserService {
         return throwError(() => new Error('UserService.resetPassword(): error resetting password: ' + err));
       })
     );
-  }
-
-  getHttpOptions() {
-    let options = {
-      headers: {
-        Authorization: 'Basic ' + this.auth.getCredentials(),
-        'X-Requested-With': 'XMLHttpRequest',
-      },
-    };
-    return options;
   }
 }
