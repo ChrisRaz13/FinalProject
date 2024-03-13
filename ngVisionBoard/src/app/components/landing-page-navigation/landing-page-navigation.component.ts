ab58442f64cfe6baef3d3fbb9062e5c754519658
MaceGibson <mace_gibson@yahoo.com>
2024-03-13 11:03:09 -0400

    updated navigation to flow with user login

--- ngVisionBoard/src/app/components/landing-page-navigation/landing-page-navigation.component.ts
+++ ngVisionBoard/src/app/components/landing-page-navigation/landing-page-navigation.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page-navigation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './landing-page-navigation.component.html',
   styleUrl: './landing-page-navigation.component.css'
 })
